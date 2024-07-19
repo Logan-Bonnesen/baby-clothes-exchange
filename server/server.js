da153b90934151b2cffbc5c4e418ded92018a877
Logan-Bonnesen <91345048+Logan-Bonnesen@users.noreply.github.com>
2024-07-19 11:44:58 -0600

    updating previous server.js commit, needed to actually add imported connectDB function

--- server/server.js
+++ server/server.js
@@ -2,10 +2,13 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv')
+const connectDB = require('./config/db')
 
 dotenv.config()
 
 const app = express()
+
+connectDB()
 
 app.use(bodyParser.json())
 
@@ -15,13 +18,13 @@ app.get('/', (req, res) => {
     res.send('Welcome to the Baby Clothes Exchange')
 })
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true, 
-}).then(() => {
-    console.log('Connected to MongoDB');
-}).catch(err => {
-    console.error(err)
-})
+// mongoose.connect(process.env.MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true, 
+// }).then(() => {
+//     console.log('Connected to MongoDB');
+// }).catch(err => {
+//     console.error(err)
+// })
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
