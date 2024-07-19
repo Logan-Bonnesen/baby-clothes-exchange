2e89f1b53b324be8d772d18421b927e24d1da91e
Logan-Bonnesen <91345048+Logan-Bonnesen@users.noreply.github.com>
2024-07-19 17:17:53 -0600

    adding routes for CRUD for items

---
+++ server/routes/itemRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+const { createItem, getItems, getItemById, updateItem, deleteItem } = require('../controllers/itemController')
+const auth = require('../middleware/auth')
+
+// create new item
+router.post('/', auth, createItem)
+
+// get all items
+router.get('/', getItems)
+
+// get item by ID
+router.get('/:id', getItemById)
+
+// update item
+router.put('/:id', updateItem)
+
+// delete item
+router.delete('/:id', auth, deleteItem)
+
