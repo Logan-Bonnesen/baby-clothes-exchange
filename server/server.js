12f2de2a0151930594ae5afa51ffae282f567c7a
Logan-Bonnesen <91345048+Logan-Bonnesen@users.noreply.github.com>
2024-07-23 12:05:26 -0600

    updaing for deployment

--- server/server.js
+++ server/server.js
@@ -48,4 +48,4 @@ app.get('/', (req, res) => {
 //     console.error(err)
 // })
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+app.listen(PORT, '0.0.0.0', () => console.log(`Server is running on port ${PORT}`))
