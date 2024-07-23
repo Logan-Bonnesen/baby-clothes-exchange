43212c65cd6081af5becb3c6c4c46f7b88ce5741
Logan-Bonnesen <91345048+Logan-Bonnesen@users.noreply.github.com>
2024-07-23 09:34:14 -0600

    adding AuthProvider wrapper to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
 import './App.css';
-
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import Header from './components/Header'
+import Footer from './components/Footer'
 import Home from './components/Home'
 import Login from './components/Login'
 import Register from './components/Register'
 import Profile from './components/Profile'
 import Items from './components/Items'
 import Logout from './components/Logout'
+import { AuthProvider } from './AuthContext';
 
 
 function App() {
   return (
+    <AuthProvider>
       <Router>
-          <Navbar />
+          <Header />
+          <div className="container">
           <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
@@ -23,7 +26,10 @@ function App() {
               <Route path="/items" element={<Items />} />
               <Route path="/logout" element={<Logout />} />
           </Routes>
+          </div>
+          <Footer />
       </Router>
+    </AuthProvider>
   );
 }
 
