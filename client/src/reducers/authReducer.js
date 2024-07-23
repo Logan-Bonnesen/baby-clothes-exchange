b4fa05f90a99d6537bba60b5130a094dffc70587
Logan-Bonnesen <91345048+Logan-Bonnesen@users.noreply.github.com>
2024-07-23 09:38:54 -0600

    updating LOGOUT to add user:null

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -48,6 +48,7 @@ const authReducer = (state = initialState, action) => {
             return {
                 ...state, 
                 token: null, 
+                user: null,
                 isAuthenticated: false,
                 loading: false
             }
