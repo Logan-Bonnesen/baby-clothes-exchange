243bfb582aad1503eeeeb704a1bbd169ff4ab10a
Logan-Bonnesen <91345048+Logan-Bonnesen@users.noreply.github.com>
2024-07-23 09:29:47 -0600

    adding GET_USER_ITEMS and ITEM_ERROR

--- client/src/actions/actionTypes.js
+++ client/src/actions/actionTypes.js
@@ -14,3 +14,5 @@ export const ADD_ITEM = 'ADD_ITEM';
 export const DELETE_ITEM = 'DELETE_ITEM';
 export const UPDATE_ITEM = 'UPDATE_ITEM';
 export const ITEM_LOADING = 'ITEM_LOADING';
+export const GET_USER_ITEMS = 'GET_USER_ITEMS';
+export const ITEMS_ERROR = 'ITEMS_ERROR'
